3415516d772f4279f79171bdcedc3a2f431b1ca2
Xopek <nakit21@gmail.com>
2021-12-04 21:18:56 +0200

    constructor correction and add mint

--- contracts/nftSale.sol
+++ contracts/nftSale.sol
@@ -2,35 +2,31 @@
 
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract nftSale is Ownable {
+    ERC721 public token;
     address public addr;
-    IERC721 public token;
     address public getPayment;
-    uint public maxAmount;
-    uint public totalSupply;
-    uint public price;
+    uint public maxAmount = 10;
+    uint public totalSupply = 100;
+    uint public price = 0.01 ether;
 
-    constructor(address _addr) {
-        token = IERC721(addr);
-        getPayment = _addr;
-        price = 0.01 ether;
-        maxAmount = 10;
-        totalSupply = 100;
+    constructor(address wallet, address _token) {
+        addr = _token;
+        token = ERC721(_token);
+        getPayment = wallet;
     }
     
-    function sellToken(uint amount) payable external {
-        require(amount <= maxAmount);
+    function buyToken(uint amount) payable external {
+        require(amount <= maxAmount && totalSupply >= amount);
         require(msg.value == price * amount);
-        require(totalSupply >= amount);
 
         (bool success,) = getPayment.call{value: msg.value}("");
 
         if(success) {
-            token.approve(msg.sender, amount);
-            token.transferFrom(addr, msg.sender, amount);
+            token.mint(msg.sender, amount);
         }
     }
     function setPrice(uint _price) external onlyOwner {
