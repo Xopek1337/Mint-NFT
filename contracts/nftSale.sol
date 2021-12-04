f1ef354c5df035294496e9e3729a6a41c2b61add
Xopek <nakit21@gmail.com>
2021-12-04 21:41:20 +0200

    change Buy function

--- contracts/nftSale.sol
+++ contracts/nftSale.sol
@@ -7,14 +7,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract nftSale is Ownable {
     ERC721 public token;
-    address public addr;
-    address public getPayment;
+    address payable public getPayment;
     uint public maxAmount = 10;
     uint public totalSupply = 100;
     uint public price = 0.01 ether;
 
-    constructor(address wallet, address _token) {
-        addr = _token;
+    constructor(address payable wallet, address _token) {
         token = ERC721(_token);
         getPayment = wallet;
     }
@@ -23,22 +21,23 @@ contract nftSale is Ownable {
         require(amount <= maxAmount && totalSupply >= amount);
         require(msg.value == price * amount);
 
-        (bool success,) = getPayment.call{value: msg.value}("");
-
-        if(success) {
-            token.mint(msg.sender, amount);
-        }
+        getPayment.transfer(amount);
+        token.mint(msg.sender, amount);
     }
+
     function setPrice(uint _price) external onlyOwner {
         price = _price;
     }
+
     function setTotalSupply(uint _supply) external onlyOwner {
         totalSupply = _supply;
     }
+
     function setMaxAmount(uint _amount) external onlyOwner {
         maxAmount = _amount;
     }
-    function setPayAddress(address _getPayment) external onlyOwner {
+
+    function setPayAddress(address payable _getPayment) external onlyOwner {
         getPayment = _getPayment;
     }
 }
