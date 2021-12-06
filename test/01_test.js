d76e02882639b4018ead3d7c0673a4254e60c3f3
Xopek <nakit21@gmail.com>
2021-12-06 12:53:30 +0200

    addition of tests

--- test/01_test.js
+++ test/01_test.js
@@ -7,7 +7,7 @@ const {
 } = require("hardhat");
 
 describe('nftTest', function () {
-  it('', async () => {
+  it('should mint 1 token and transfer it to account', async () => {
     const [wallet, addr1] = await ethers.getSigners();
     let price = await BigNumber.from('10000000000000000');
 
@@ -26,5 +26,22 @@ describe('nftTest', function () {
     const endingBalance = await ethers.provider.getBalance(wallet.address);
 
     expect(startingBalance).to.equal(endingBalance.sub(price));
+  });
+  it('should put 200 token in totalAmount', async () => {
+    const [wallet, addr1] = await ethers.getSigners();
+
+    const ERC721Instance = await ethers.getContractFactory("ERC721Mint");
+    const ERC721 = await ERC721Instance.deploy("BasicToken","BST");
+    await ERC721.deployed();
+
+    const nftSaleInstance = await ethers.getContractFactory("nftSale");
+    const nftSale = await nftSaleInstance.deploy(wallet.address, ERC721.address);
+    await nftSale.deployed();
+
+    const amount = 200;
+    await nftSale.setTotalSellAmount(amount);
+    const endingTotalSellAmount = await nftSale.totalSellAmount();
+    console.log(endingTotalSellAmount);
+    expect(amount).to.equal(endingTotalSellAmount);
   });
 });
