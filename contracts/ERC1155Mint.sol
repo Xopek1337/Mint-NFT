2bf56589a5d582de4e88cb69c07f5f69950b23f1
Xopek <nakit21@gmail.com>
2021-12-23 00:10:50 +0200

    add ownable

--- contracts/ERC1155Mint.sol
+++ contracts/ERC1155Mint.sol
@@ -3,12 +3,13 @@
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC1155Mint is ERC1155 {
+contract ERC1155Mint is ERC1155, Ownable {
     constructor(string memory uri) ERC1155(uri) {
     }
 
-    function mint(uint256 collectionId, uint256 amount, address addr) public payable returns (bool) {
+    function mint(uint256 collectionId, uint256 amount, address addr) public payable onlyOwner returns (bool) {
         _mint(addr, collectionId, amount, "");
 
         return true;
