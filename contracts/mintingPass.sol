52a8fb45a9ad235d1efa96dada29b449b2dc56d1
Xopek <nakit21@gmail.com>
2022-01-02 12:54:03 +0200

    gg

--- contracts/mintingPass.sol
+++ contracts/mintingPass.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.10;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 
 /**
  * @title  mintingPass.
