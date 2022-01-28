7ae94cf8c528e99b45a397ae77a2c510e1de62ea
Anatoly Antsupov <NAnatolyAntsupovN@yandex.ru>
2022-01-29 00:28:58 +0300

    some fixes

--- contracts/ERC721Mint.sol
+++ contracts/ERC721Mint.sol
@@ -1,83 +1,65 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity 0.8.11;
 
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '../node_modules/@openzeppelin/contracts/access/Ownable.sol';
+import '../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '../node_modules/@openzeppelin/contracts/access/AccessControl.sol';
 
-contract ERC721Mint is ERC721Enumerable, Ownable {
-    address public minter;
-    address public burner;
+contract ERC721Mint is ERC721Enumerable, Ownable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
+
     string public uri;
 
     uint public tokenId = 0;
 
-    mapping(uint => string) private _tokenURIs;
-
     constructor(string memory _name, string memory _symbol, string memory _uri) ERC721 (_name, _symbol) {
         uri = _uri;
-    }
-
-    modifier onlyMinter() {
-        require(
-            msg.sender == minter,
-            'ERC721Mint: sender is not minter'
-        );
-        _;
-    }
-
-    modifier onlyBurner() {
-        require(
-            msg.sender == burner,
-            'ERC721Mint: sender is not burner'
-        );
-        _;
+        
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function _setMinter(address _minter) 
         external 
-        onlyOwner 
+        onlyRole(DEFAULT_ADMIN_ROLE) 
         returns (bool) 
     {
-        minter = _minter;
+        grantRole(MINTER_ROLE, _minter);
 
         return true;
     }
 
      function _setBurner(address _burner) 
         external 
-        onlyOwner 
+        onlyRole(DEFAULT_ADMIN_ROLE) 
         returns (bool) 
     {
-        burner = _burner;
+        grantRole(BURNER_ROLE, _burner);
 
         return true;
     }
 
     function mint(address to) 
         external 
-        onlyMinter
-        returns (uint) 
+        onlyRole(MINTER_ROLE)
+        returns (bool) 
     {
         tokenId++;
 
         _mint(to, tokenId);
 
-        return tokenId;
+        return true;
     }
 
     function burn(uint _tokenId) 
         external 
-        onlyBurner
+        onlyRole(BURNER_ROLE)
         returns (bool) 
     {
         require(_isApprovedOrOwner(_msgSender(), _tokenId), 'ERC721Mint::burn: caller is not owner nor approved');
 
         _burn(_tokenId);
-
-        if (bytes(_tokenURIs[_tokenId]).length != 0) {
-            delete _tokenURIs[_tokenId];
-        }
 
         return true;
     }
@@ -85,24 +67,10 @@ contract ERC721Mint is ERC721Enumerable, Ownable {
     function tokenURI(uint _tokenId) 
         public 
         view 
-        override 
+        override
         returns (string memory) 
     {
-        require(_exists(_tokenId), 'ERC721Mint::tokenURI: URI query for nonexistent token');
-
-        string memory _tokenURI = _tokenURIs[_tokenId];
-        string memory base = _baseURI();
-
-        if (bytes(base).length == 0) {
-            return _tokenURI;
-        }
-        if (bytes(_tokenURI).length > 0) {
-            return string(abi.encodePacked(base, _tokenURI));
-            //return string(abi.encodePacked(base, '/', _tokenURI));
-        }
-
         return super.tokenURI(_tokenId);
-        //return string(abi.encodePacked(_baseURI(), '/', Strings.toString(_tokenId)));
     }
 
      function _baseURI() 
@@ -114,34 +82,32 @@ contract ERC721Mint is ERC721Enumerable, Ownable {
         return uri;
     }
 
-    function _setTokenURI(uint _tokenId, string memory _tokenURI) 
-        internal 
-        virtual 
+    function _setNewURI(string memory _newURI) 
+        external
         onlyOwner
         returns (bool) 
     {
-        require(_exists(_tokenId), 'ERC721Mint::_setTokenURI: URI set of nonexistent token');
-        _tokenURIs[_tokenId] = _tokenURI;
+        uri = _newURI;
 
         return true;
     }
 
-    function _withdrawERC20(IERC20 tokenContract, address recepient) 
+    function _withdrawERC20(IERC20 tokenContract, address recepient)
         external 
         onlyOwner 
-        returns(bool)
+        returns(bool) 
     {
         tokenContract.transfer(recepient, tokenContract.balanceOf(address(this)));
 
         return true;
     }
 
-    function _withdrawERC721(IERC721 tokenContract, address recepient, uint tokenId) 
+    function _withdrawERC721(IERC721 tokenContract, address recepient, uint _tokenId)
         external 
         onlyOwner 
         returns(bool) 
     {
-        tokenContract.transferFrom(address(this), recepient, tokenId);
+        tokenContract.transferFrom(address(this), recepient, _tokenId);
 
         return true;
     }
