15fe5438f290b27bf04f05e8792d708a0df89531
Anatoly Antsupov <NAnatolyAntsupovN@yandex.ru>
2022-01-28 19:42:59 +0300

    2 v

--- contracts/ERC721Mint.sol
+++ contracts/ERC721Mint.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import '../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '../node_modules/@openzeppelin/contracts/access/Ownable.sol';
 
 contract ERC721Mint is ERC721Enumerable, Ownable {
     address public manager;
@@ -10,7 +10,7 @@ contract ERC721Mint is ERC721Enumerable, Ownable {
 
     uint public tokenId = 0;
 
-    mapping(uint256 => string) private _tokenURIs;
+    mapping(uint => string) private _tokenURIs;
 
     constructor(string memory _name, string memory _symbol, string memory _uri) ERC721 (_name, _symbol) {
         uri_ = _uri;
@@ -19,28 +19,39 @@ contract ERC721Mint is ERC721Enumerable, Ownable {
     modifier onlyManager() {
         require(
             msg.sender == manager,
-            'test: sender is not manager'
+            'ERC721Mint: sender is not manager'
         );
         _;
     }
 
-    function setManager(address _manager) external onlyOwner returns (bool) {
+    function setManager(address _manager) 
+        external 
+        onlyOwner 
+        returns (bool) 
+    {
         manager = _manager;
 
         return true;
     }
 
-    function mint(address to) external onlyManager returns (uint) {
+    function mint(address to) 
+        external 
+        onlyManager 
+        returns (bool) 
+    {
         tokenId++;
 
         _mint(to, tokenId);
 
-        return tokenId;
+        return true;
     }
 
-    function burn(uint256 tokenId) external onlyManager returns (bool) {
-        //require(tokenId > 10000, 'test: tokenId is insufficient for burning'); тут чет с getBot
-        require(_isApprovedOrOwner(_msgSender(), tokenId), 'test: caller is not owner nor approved');
+    function burn(uint tokenId) 
+        external 
+        onlyManager 
+        returns (bool) 
+    {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), 'ERC721Mint::burn: caller is not owner nor approved');
 
         _burn(tokenId);
 
@@ -51,8 +62,13 @@ contract ERC721Mint is ERC721Enumerable, Ownable {
         return true;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), 'test: URI query for nonexistent token');
+    function tokenURI(uint tokenId) 
+        public 
+        view 
+        override 
+        returns (string memory) 
+    {
+        require(_exists(tokenId), 'ERC721Mint::tokenURI: URI query for nonexistent token');
 
         string memory _tokenURI = _tokenURIs[tokenId];
         string memory base = _baseURI();
@@ -68,13 +84,23 @@ contract ERC721Mint is ERC721Enumerable, Ownable {
         return super.tokenURI(tokenId);
     }
 
-     function _baseURI() internal view virtual override returns (string memory) {
+     function _baseURI() 
+        internal 
+        view 
+        override
+        returns (string memory) 
+    {
         return uri_;
     }
 
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
-        require(_exists(tokenId), 'test: URI set of nonexistent token');
+    function _setTokenURI(uint tokenId, string memory _tokenURI) 
+        internal 
+        virtual 
+        returns (bool) 
+    {
+        require(_exists(tokenId), 'ERC721Mint::_setTokenURI: URI set of nonexistent token');
         _tokenURIs[tokenId] = _tokenURI;
-    }
 
+        return true;
+    }
 }
