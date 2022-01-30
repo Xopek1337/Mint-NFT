76b347c81407e8f7822759d76679af7962539be5
Anatoly Antsupov <NAnatolyAntsupovN@yandex.ru>
2022-01-30 16:14:08 +0300

    add script

---
+++ scripts/04_setNewURI_ERC721Mint.js
@@ -0,0 +1,22 @@
+const network = hre.network.name;
+const fs = require("fs");
+
+async function main() {
+  const newURI = process.env.NFT_URI;
+
+  const dir = "./networks/";
+  const fileName = "ERC721Mint_" + `${network}.json`;
+  const data = JSON.parse(await fs.readFileSync(dir + fileName, { encoding: "utf8" }));
+
+  const ERC721MintInstance = await ethers.getContractFactory("ERC721Mint");
+  const ERC721Mint = await ERC721MintInstance.attach(data.ERC721Mint);
+
+  await ERC721Mint._setNewURI(newURI);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
